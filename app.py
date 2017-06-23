3a3a7812f80ab7c66be3edf6743cfc685f687831
Bryan Swagerty <bryantee@gmail.com>
2017-06-22 19:38:13 -0700

    Add endpoints to feed pet and get datetime last fed

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 #!petweb/bin/flask
-from flask import Flask
+from flask import Flask, jsonify
+import datetime
 
 app = Flask(__name__)
+
+times_fed = []
 
 @app.route('/')
 def index():
     return 'hello world'
+
+@app.route('/feed', methods=['POST'])
+def feed():
+    new_date = datetime.datetime.now()
+    times_fed.append(new_date)
+    # print(times_fed)
+    # return jsonify({'times_fed': times_fed})
+    return 'Nom nom nom...'
+
+@app.route('/lastfed', methods=['GET'])
+def last_fed():
+    return jsonify({'lastFed': times_fed[-1]})
 
 if __name__ == '__main__':
     app.run(debug=True)
