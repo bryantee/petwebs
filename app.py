92e170b8dcdf59df81b224ffdbc69c7f21a1de8f
Bryan Swagerty <bryantee@gmail.com>
2017-06-22 19:12:38 -0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,11 @@
+#!petweb/bin/flask
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'hello world'
+
+if __name__ == '__main__':
+    app.run(debug=True)
